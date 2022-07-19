793aa5771f26c0a1087e7f49cba1c36da246adc4
nguyen-thanh-tu <nguyenthanhtu17032000@gmail.com>
2022-07-19 16:14:46 +0700

    create file php by cml

--- Tunt/QuickCommands/Console/CreatePlugin.php
+++ Tunt/QuickCommands/Console/CreatePlugin.php
@@ -65,7 +65,9 @@ class CreatePlugin extends Command
             ($this->pluginClass = $input->getOption(self::PLUGIN_ClASS))
         )
         {
+            $pathAppCode = $this->dir->getPath('app').'/code';
             $this->generatedDiXML();
+            $this->generatedPluginPHP($pathAppCode);
         }
         return $this;
     }
@@ -79,10 +81,77 @@ class CreatePlugin extends Command
             fwrite($registrationFile, $this->getDiXMLContent());
             fclose($registrationFile);
         }else{
+            $xml = <<<XML
+    <type name="%s">
+        <plugin name="%s" type="%s" />
+    </type>\n
+XML;
+            $xmlSnippet = sprintf($xml, $this->className, $this->pluginName, $this->pluginClass);
             $file = simplexml_load_file($path);
             $events = (array)$file;
+            $dom = new \DOMDocument();
+            $dom->loadXML($file->asXML());
+            $fragment = $dom->createDocumentFragment();
+            $norender = false;
+            if(!empty($events['type']))
+            {
+                if(
+                    !empty(((array)$events['type'])['@attributes']) &&
+                    !empty(((array)$events['type'])['plugin'])
+                )
+                {
+                    $events['type'] = [
+                        0 => [
+                            '@attributes' => ((array)$events['type'])['@attributes'],
+                            'plugin'      => ((array)$events['type'])['plugin']
+                            ]
+                    ];
+                }
+                foreach($events['type'] as $key => $type)
+                {
+                    $type = (array)$type;
+                    if(
+                        !empty($type['plugin']) &&
+                        $type['@attributes']['name'] == $this->className &&
+                        ((array)$type['plugin'])['@attributes']['type'] == $this->pluginClass
+                    )
+                    {
+                        $norender = true;
+                        break;
+                    }
+                }
+            }
+            if(!$norender)
+            {
+                $fragment->appendXML($xmlSnippet);
+                $dom->documentElement->appendChild($fragment);
+                $dom->save($path);
+            }
         }
         return $this;
+    }
+
+    public function generatedPluginPHP($paths)
+    {
+        $folders = explode('\\',$this->pluginClass);
+        $namespace = '';
+        foreach($folders as $key => $folder)
+        {
+            $paths .= '/'.$folder;
+            if(empty(is_file($paths.'.php')) && $key == (count($folders) - 1))
+            {
+                $namespace = rtrim($namespace,'\\');
+                $class = $folder;
+                $registrationFile = fopen($paths.'.php', 'w');
+                fwrite($registrationFile, $this->getContentPluginPHP($namespace,$class));
+                fclose($registrationFile);
+                break;
+            }elseif(empty(is_dir($paths)) && $key != (count($folders) - 1))
+            {
+                mkdir($paths);
+            }
+            $namespace .= $folder.'\\';
+        }
     }
 
     public function getDiXMLContent()
@@ -98,4 +167,64 @@ class CreatePlugin extends Command
         return $content;
     }
 
+    public function getContentPluginPHP($namespace,$class)
+    {
+        $functions = '';
+        $param = '';
+        $reflect = new \ReflectionClass($this->className);
+        if(!$reflect->isFinal()){
+            $interfaces = class_implements($this->className);
+            if(empty($interfaces['Magento\Framework\ObjectManager\NoninterceptableInterface']))
+            {
+                $classMethods = get_class_methods($this->className);
+                foreach($classMethods as $method)
+                {
+                    if(in_array($method,['__construct','__destruct']))
+                    {
+                        continue;
+                    }
+                    $classMethod = new \ReflectionMethod($this->className,$method);
+                    if($classMethod->isPublic())
+                    {
+                        if($classMethod->isStatic() || $classMethod->isFinal()){
+                            continue;
+                        }
+                        $paramaters = $classMethod->getParameters();
+                        foreach($paramaters as $paramater)
+                        {
+                            $param .= ', $'.$paramater->name;
+                        }
+                        $functions .= '
+    // plugin '.$method.'
+    public function before'.ucfirst($method).'('.'\\'.$this->className.' $subject'.$param.')
+    {
+        return ['.ltrim($param,', ').'];
+    }
+
+    public function around'.ucfirst($method).'('.'\\'.$this->className.' $subject, callable $proceed'.$param.')
+    {
+    }
+
+    public function after'.ucfirst($method).'('.'\\'.$this->className.' $subject, $result)
+    {
+        return $result;
+    }
+    ';
+                        $param = '';
+                    }
+                }
+            }
+        }
+
+        $content = '<?php
+
+namespace '.$namespace.';
+
+class '.$class.'
+{
+    '.$functions.'
+}';
+        return $content;
+    }
 }
+
